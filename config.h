5e1984830c016a55c4a768dc162f4294e5d3c738
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2023-04-06 15:16:41 +0300

    add  overlays.pc, config.pc.h

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* interval between updates (in ms) */
-const unsigned int interval = 1000;
+const unsigned int interval = 5000;
 
 /* text to show if no value can be retrieved */
 static const char unknown_str[] = "---";
@@ -9,72 +9,17 @@ static const char unknown_str[] = "---";
 /* maximum output string length */
 #define MAXLEN 2048
 
-/*
- * function            description                     argument (example)
- *
- * battery_perc        battery percentage              battery name (BAT0)
- *                                                     NULL on OpenBSD/FreeBSD
- * battery_remaining   battery remaining HH:MM         battery name (BAT0)
- *                                                     NULL on OpenBSD/FreeBSD
- * battery_state       battery charging state          battery name (BAT0)
- *                                                     NULL on OpenBSD/FreeBSD
- * cat                 read arbitrary file             path
- * cpu_freq            cpu frequency in MHz            NULL
- * cpu_perc            cpu usage in percent            NULL
- * datetime            date and time                   format string (%F %T)
- * disk_free           free disk space in GB           mountpoint path (/)
- * disk_perc           disk usage in percent           mountpoint path (/)
- * disk_total          total disk space in GB          mountpoint path (/)
- * disk_used           used disk space in GB           mountpoint path (/)
- * entropy             available entropy               NULL
- * gid                 GID of current user             NULL
- * hostname            hostname                        NULL
- * ipv4                IPv4 address                    interface name (eth0)
- * ipv6                IPv6 address                    interface name (eth0)
- * kernel_release      `uname -r`                      NULL
- * keyboard_indicators caps/num lock indicators        format string (c?n?)
- *                                                     see keyboard_indicators.c
- * keymap              layout (variant) of current     NULL
- *                     keymap
- * load_avg            load average                    NULL
- * netspeed_rx         receive network speed           interface name (wlan0)
- * netspeed_tx         transfer network speed          interface name (wlan0)
- * num_files           number of files in a directory  path
- *                                                     (/home/foo/Inbox/cur)
- * ram_free            free memory in GB               NULL
- * ram_perc            memory usage in percent         NULL
- * ram_total           total memory size in GB         NULL
- * ram_used            used memory in GB               NULL
- * run_command         custom shell command            command (echo foo)
- * swap_free           free swap in GB                 NULL
- * swap_perc           swap usage in percent           NULL
- * swap_total          total swap size in GB           NULL
- * swap_used           used swap in GB                 NULL
- * temp                temperature in degree celsius   sensor file
- *                                                     (/sys/class/thermal/...)
- *                                                     NULL on OpenBSD
- *                                                     thermal zone on FreeBSD
- *                                                     (tz0, tz1, etc.)
- * uid                 UID of current user             NULL
- * uptime              system uptime                   NULL
- * username            username of current user        NULL
- * vol_perc            OSS/ALSA volume in percent      mixer file (/dev/mixer)
- *                                                     NULL on OpenBSD/FreeBSD
- * wifi_essid          WiFi ESSID                      interface name (wlan0)
- * wifi_perc           WiFi signal in percent          interface name (wlan0)
- */
-
 static const struct arg args[] = {
-    {netspeed_rx, "⏬%s", "wlp6s0"},
-    {netspeed_tx, "🔀%s", "wlp6s0"},
+    /* {netspeed_rx, "⏬%s", "wlp6s0"}, */
+    /* {netspeed_tx, "🔀%s", "wlp6s0"}, */
     {run_command, "🎵%s", "mpc current"},
     {run_command, "🌡️%s", "sensors | awk '/^Tctl/ {print $2}'"},
     {cpu_perc, "💻%s"},
-    {disk_perc, "💾%s", "/"},
+    /* {disk_perc, "💾%s", "/"}, */
     {run_command, "%s", "cat /tmp/wttr"},
     {run_command, "👾%s  ", "cat /tmp/ip"},
     {datetime, "🗓️%s", "%B %d %A %R"},
-    {keymap, "⌨️ %s"},
-    {battery_perc, "🔋%s", "BAT0"},
-    {wifi_essid, "📡%s", "wlp6s0"},
-    {wifi_perc, "📶%s", "wlp6s0"}};
+    {keymap, "⌨️ %s"}};
+    /* {battery_perc, "🔋%s", "BAT0"}, */
+    /* {wifi_essid, "📡%s", "wlp6s0"}, */
+    /* {wifi_perc, "📶%s", "wlp6s0"}}; */
